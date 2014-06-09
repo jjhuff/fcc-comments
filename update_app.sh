fb456c343ddb179ad8f2aa0c98371186968b6bcd
Justin Huff <jjhuff@mspin.net>
2014-06-09 12:10:13 -0700

    Add needed index

--- update_app.sh
+++ update_app.sh
@@ -3,3 +3,4 @@ APP=$( dirname "${BASH_SOURCE[0]}" )
 ~/google_appengine/appcfg.py --oauth2 update_queues .
 ~/google_appengine/appcfg.py --oauth2 update app.yaml batch.yaml
 ~/google_appengine/appcfg.py --oauth2 update_cron .
+#~/google_appengine/appcfg.py --oauth2 update_indexes .
